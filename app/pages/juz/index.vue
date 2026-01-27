480424fb776c1c647079bed59c7398b7e4998742
Syed Hassan Raza <hassanraza@satjapan.co>
2026-01-27 18:51:05 +0500

    new changes

--- app/pages/juz/index.vue
+++ app/pages/juz/index.vue
@@ -4,14 +4,16 @@
       <v-col cols="12"> Quran Juz </v-col>
       <v-col cols="12">
         <v-card class="mx-auto pa-4" outlined>
-           <v-chip color="green" 
-              class="ma-2"
-              v-for="n in 30"
-              :key="n"
-              :title="'Item ' + n"
-              @click="goto(`/juz/${n}`)">
-            Juz#  {{ n }}
-            </v-chip>
+          <v-chip
+            color="green"
+            class="ma-2"
+            v-for="n in 30"
+            :key="n"
+            :title="'Item ' + n"
+            @click="goto(`/juz/${n}`)"
+          >
+            Juz# {{ n }}
+          </v-chip>
         </v-card>
       </v-col>
     </v-row>

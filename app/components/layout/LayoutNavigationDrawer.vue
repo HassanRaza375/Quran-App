a2eee148ec657cf1db5f52d2f06726d63e9d45ac
Syed Hassan Raza <105318011+HassanRaza375@users.noreply.github.com>
2026-01-27 23:53:07 +0500

    search page new changes

--- app/components/layout/LayoutNavigationDrawer.vue
+++ app/components/layout/LayoutNavigationDrawer.vue
@@ -1,13 +1,31 @@
 <template>
   <v-navigation-drawer :model-value="modelValue" app @update:model-value="emit('update:modelValue', $event)">
-    <v-list>
-      <v-list-item @click="goto('/')" title="Home" prepend-icon="mdi-home" />
-      <v-list-item @click="goto('/search')" title="Search" prepend-icon="mdi-magnify" />
-      <v-list-item @click="goto('/bookmarks')" title="Bookmarks" prepend-icon="mdi-bookmark" />
-      <v-list-item @click="goto('/juz')" title="Juz" prepend-icon="mdi-book" />
-      <v-list-item @click="goto('/sajda')" title="Sajda" prepend-icon="mdi-airbag" />
-      <v-list-item @click="goto('/settings')" title="Settings" prepend-icon="mdi-cog" />
-      <v-list-item @click="goto('/about')" title="About" prepend-icon="mdi-information" />
+    <v-list nav density="comfortable">
+
+      <v-list-item title="Home" prepend-icon="mdi-home" @click="goto('/')" />
+
+      <v-list-item title="Search" prepend-icon="mdi-magnify" @click="goto('/search')" />
+
+      <v-list-item title="Bookmarks" prepend-icon="mdi-bookmark" @click="goto('/bookmarks')" />
+
+      <!-- JUZ COLLAPSIBLE -->
+      <v-list-group value="juz">
+        <!-- Activator -->
+        <template #activator="{ props }">
+          <v-list-item v-bind="props" title="Juz" prepend-icon="mdi-book" />
+        </template>
+
+        <!-- Children -->
+        <v-list-item v-for="juz in 30" :key="juz" :title="`Juz ${juz}`" prepend-icon="mdi-chevron-right"
+          @click="goto(`/juz/${juz}`)" />
+      </v-list-group>
+
+      <v-list-item title="Sajda" prepend-icon="mdi-airbag" @click="goto('/sajda')" />
+
+      <v-list-item title="Settings" prepend-icon="mdi-cog" @click="goto('/settings')" />
+
+      <v-list-item title="About" prepend-icon="mdi-information" @click="goto('/about')" />
+
     </v-list>
   </v-navigation-drawer>
 </template>
@@ -30,3 +48,4 @@ const props = defineProps({
 
 const emit = defineEmits(['update:modelValue'])
 </script>
+<style scoped></style>
